62b8a0779d844e2c0d9ab09a6f33066e67a25457
bhavanapanduru <p.bhavana1997@gmail.com>
2019-02-23 23:13:15 +0530

    [Bhavana] Configure Jest with the application 'Insta'

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -22,4 +21,12 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(HeaderComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('INSTA');
+  });
+
 });
